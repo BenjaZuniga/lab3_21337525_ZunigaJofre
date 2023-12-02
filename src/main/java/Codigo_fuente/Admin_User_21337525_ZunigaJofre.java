c9079d4e448385f8aef90034a7386cfd8db355a0
Benja1407 <benjamin.zuniga.j@usach.cl>
2023-12-02 17:38:17 -0300

    Doc Users

--- src/main/java/Codigo_fuente/Admin_User_21337525_ZunigaJofre.java
+++ src/main/java/Codigo_fuente/Admin_User_21337525_ZunigaJofre.java
@@ -1,7 +1,10 @@
 package Codigo_fuente;
 
 public class Admin_User_21337525_ZunigaJofre extends User_21337525_ZunigaJofre{
-
+    /**
+     * Constructor de un Admin_User
+     * @param name String
+     */
     public Admin_User_21337525_ZunigaJofre(String name){
         super(name, 1);
     }
--- src/main/java/Codigo_fuente/Common_User_21337525_ZunigaJofre.java
+++ src/main/java/Codigo_fuente/Common_User_21337525_ZunigaJofre.java
@@ -1,7 +1,10 @@
 package Codigo_fuente;
 
 public class Common_User_21337525_ZunigaJofre extends User_21337525_ZunigaJofre{
-
+    /**
+     * Constructor de un Common_User
+     * @param name String
+     */
     public Common_User_21337525_ZunigaJofre(String name) {
         super(name,0);
     }
