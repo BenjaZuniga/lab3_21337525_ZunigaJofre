9fe69b5dfa2be288c14784b5ee004e7a4c09b092
Benja1407 <benjamin.zuniga.j@usach.cl>
2023-12-03 22:22:59 -0300

    System doc

--- src/main/java/Codigo_fuente/System_21337525_ZunigaJofre.java
+++ src/main/java/Codigo_fuente/System_21337525_ZunigaJofre.java
@@ -3,145 +3,224 @@ package Codigo_fuente;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 public class System_21337525_ZunigaJofre implements System_Interface_21337525_ZunigaJofre{
-    private String Name;
-    private int InicialChatbotCodeLink;
-    private List<User_21337525_ZunigaJofre> RegisterUsers;
-    private List<String> LoginUser;
-    private List<Option_21337525_ZunigaJofre> ActualOptions;
-    private List<Chatbot_21337525_ZunigaJofre> Chatbots;
+    private String name;
+    private int inicialChatbotCodeLink;
+    private List<User_21337525_ZunigaJofre> registerUsers;
+    private List<String> loginUser;
+    private List<Option_21337525_ZunigaJofre> actualOptions;
+    private List<Chatbot_21337525_ZunigaJofre> chatbots;
 
+    /**
+     * Constructor de System
+     * @param name int
+     * @param inicialChatbotCodeLink String
+     * @param chatbots List<Chatbot_21337525_ZunigaJofre>
+     */
     public System_21337525_ZunigaJofre(String name, int inicialChatbotCodeLink, List<Chatbot_21337525_ZunigaJofre> chatbots){
-        Name = name;
-        InicialChatbotCodeLink = inicialChatbotCodeLink;
-        RegisterUsers = new ArrayList<>();
-        LoginUser = new ArrayList<>();
-        ActualOptions = new ArrayList<>();
-        ArrayList<Integer> cbscodes = new ArrayList<>();
+        this.name = name;
+        this.inicialChatbotCodeLink = inicialChatbotCodeLink;
+        this.registerUsers = new ArrayList<>();
+        this.loginUser = new ArrayList<>();
+        this.actualOptions = new ArrayList<>();
+        ArrayList<Integer> chatbotsCodes = new ArrayList<>();
         ArrayList<Chatbot_21337525_ZunigaJofre> newChatbots = new ArrayList<>();
         for(Chatbot_21337525_ZunigaJofre chatbot: chatbots){
-            int code = chatbot.getId();
-            if(!cbscodes.contains(code)){
-                cbscodes.add(code);
+            int chatbotCode = chatbot.getId();
+            if(!chatbotsCodes.contains(chatbotCode)){
+                chatbotsCodes.add(chatbotCode);
                 newChatbots.add(chatbot);
             }
         }
-        Chatbots = newChatbots;
+        this.chatbots = newChatbots;
     }
 
-    @Override
+    /**
+     * Método que retorna el name de un System
+     * @return name
+     */
     public String getName() {
-        return Name;
+        return name;
     }
 
-    @Override
+    /**
+     * Método que retorna el inicialChatbotCodeLink de un System
+     * @return inicialChatbotCodeLink
+     */
     public int getInicialChatbotCodeLink() {
-        return InicialChatbotCodeLink;
+        return inicialChatbotCodeLink;
     }
 
-    @Override
+    /**
+     * Método que retorna el registerUsers de un System
+     * @return registerUsers
+     */
     public List<User_21337525_ZunigaJofre> getRegisterUsers() {
-        return RegisterUsers;
+        return registerUsers;
     }
 
-    @Override
-    public List<String> getLoginUser() {
-        return LoginUser;
-    }
-
-    @Override
-    public List<Option_21337525_ZunigaJofre> getActualOptions() {
-        return ActualOptions;
-    }
-
-    @Override
-    public List<Chatbot_21337525_ZunigaJofre> getChatbots() {
-        return Chatbots;
-    }
-    public List<Integer> getChatbotsIds(){
-        List<Integer> codes = new ArrayList<>();
-        for(Chatbot_21337525_ZunigaJofre chatbot : Chatbots){
-            int code = chatbot.getId();
-            if(!codes.contains(code)){
-                codes.add(code);
+    /**
+     * Método que retorna los nombres de los usuarios registrados en un System
+     * @return registerUsersNames
+     */
+    public List<String>getRegisterUsersNames(){
+        List <String> registerUsersNames = new ArrayList<>();
+        for(User_21337525_ZunigaJofre user: registerUsers){
+            if(!registerUsersNames.contains(user.getName())){
+                registerUsersNames.add(user.getName());
             }
         }
-        return codes;
+        return registerUsersNames;
     }
 
-    @Override
+    /**
+     * Método que retorna el loginUser de un System
+     * @return loginUser
+     */
+    public List<String> getLoginUser() {
+        return loginUser;
+    }
+
+    /**
+     * Método que retorna el actualOptions de un System
+     * @return actualOptions
+     */
+    public List<Option_21337525_ZunigaJofre> getActualOptions() {
+        return actualOptions;
+    }
+
+    /**
+     * Método que retorna el loginUser de un System
+     * @return chatbots
+     */
+    public List<Chatbot_21337525_ZunigaJofre> getChatbots() {
+        return chatbots;
+    }
+
+    /**
+     * Método que retorna los ids de los chatbots de un System
+     * @return
+     */
+    public List<Integer> getChatbotsIds(){
+        List<Integer> chatbotsCodes = new ArrayList<>();
+        for(Chatbot_21337525_ZunigaJofre chatbot : chatbots){
+            int chatbotCode = chatbot.getId();
+            if(!chatbotsCodes.contains(chatbotCode)){
+                chatbotsCodes.add(chatbotCode);
+            }
+        }
+        return chatbotsCodes;
+    }
+
+    /**
+     * Método que busca un chatbot dentro de un System por id
+     * @param id int
+     * @return Chatbot
+     */
+    public Chatbot_21337525_ZunigaJofre getChatbotById(Integer id){
+        for(Chatbot_21337525_ZunigaJofre cb: chatbots){
+            Integer cbId = cb.getId();
+            if(cbId.equals(id)){
+                return cb;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Método que agrega un chatbot a un System si su id no está repetido
+     * @param chatbot Chatbot
+     */
     public void systemAddChatbot(Chatbot_21337525_ZunigaJofre chatbot) {
         List<Integer> codes = getChatbotsIds();
         if(!codes.contains(chatbot.getId())){
-            Chatbots.add(chatbot);
+            chatbots.add(chatbot);
         }
 
     }
 
-    @Override
+    /**
+     * Método que agrega un chatbot a un System si su id no está repetido
+     * @param user User
+     */
     public void systemAddUser(User_21337525_ZunigaJofre user) {
-        if(!RegisterUsers.contains(user)){
-            RegisterUsers.add(user);
+        List<String> userNames = getRegisterUsersNames();
+        if(!userNames.contains(user.getName())){
+            registerUsers.add(user);
         }
 
     }
 
-    @Override
+    /**
+     * Método que logea un usuario a un System si es que esta registrado y no hay otro logeado
+     * @param user String
+     */
     public void systemLogin(String user) {
         String name;
-        if(LoginUser.isEmpty()) {
-            for (User_21337525_ZunigaJofre systemUser : RegisterUsers) {
-                name = systemUser.getName();
-                if (user.equals(name)) {
-                    LoginUser.add(user);
-                }
+        if(loginUser.isEmpty()) {
+            List<String> userNames = getRegisterUsersNames();
+            if(!userNames.contains(user)){
+                loginUser.add(user);
             }
         }
     }
 
-    @Override
+    /**
+     * Método que deslogea un usuario de un System
+     */
     public void systemLogout() {
-        LoginUser = new ArrayList<>();
-        ActualOptions = new ArrayList<>();
+        loginUser = new ArrayList<>();
+        actualOptions = new ArrayList<>();
     }
 
-    @Override
+    /**
+     * Método que permite hablar con un System
+     * @param message String
+     */
     public void systemTalk(String message) {
-        Date date = new Date();
-        String chat;
-        chat = date + " - " + LoginUser.get(0) + ": " + message + "\n";
-        if(ActualOptions.isEmpty()){
-            Chatbot_21337525_ZunigaJofre cb = getChatbotById(InicialChatbotCodeLink);
-            chat = chat + date +  " - " + cb.getName() + "\n";
-            Flow_21337525_ZunigaJofre flow = cb.getFlowById(cb.getStartFlowId());
-            chat = chat + flow.getName() +"\n" + flow.getAllOptionMessages();
-            ActualOptions = flow.getOptions();
+        if(!loginUser.isEmpty()){
+            Date date = new Date();
+            String chat;
+            chat = date + " - " + loginUser.get(0) + ": " + message + "\n";
+            if(actualOptions.isEmpty()){
+                Chatbot_21337525_ZunigaJofre cb = getChatbotById(inicialChatbotCodeLink);
+                chat = chat + date +  " - " + cb.getName() + "\n";
+                Flow_21337525_ZunigaJofre flow = cb.getFlowById(cb.getStartFlowId());
+                chat = chat + flow.getName() +"\n" + flow.getAllOptionMessages();
+                actualOptions = flow.getOptions();
             }else{
-            for(Option_21337525_ZunigaJofre op: ActualOptions){
-                if(op.getKeyword().contains(message) ||op.getMessage().contains(message)){
-                    Chatbot_21337525_ZunigaJofre cb = getChatbotById(op.getChatbotCodeLink());
-                    chat =  chat + date + " - " + cb.getName() + "\n";
-                    Flow_21337525_ZunigaJofre flow = cb.getFlowById(op.getInicialFlowCodeLink());
-                    chat = chat + flow.getName() +"\n" + flow.getAllOptionMessages();
-                    ActualOptions = flow.getOptions();
+                for(Option_21337525_ZunigaJofre op: actualOptions){
+                    try {
+                        if(op.getKeyword().contains(message) ||op.getMessage().contains(message)){
+                            Chatbot_21337525_ZunigaJofre cb = getChatbotById(op.getChatbotCodeLink());
+                            chat =  chat + date + " - " + cb.getName() + "\n";
+                            Flow_21337525_ZunigaJofre flow = cb.getFlowById(op.getInicialFlowCodeLink());
+                            chat = chat + flow.getName() +"\n" + flow.getAllOptionMessages();
+                            actualOptions = flow.getOptions();
+                        }
+                    }catch (Exception e) {
+                        System.out.println("Ninguna opción tiene asociado el mensaje ingresado");
+                    }
+                }
+            }
+            for(User_21337525_ZunigaJofre user: registerUsers){
+                String nameUser = user.getName();
+                if(nameUser.equals(loginUser.get(0))){
+                    user.addChatHistory(chat);
                 }
             }
         }
-        for(User_21337525_ZunigaJofre user: RegisterUsers){
-            String nameUser = user.getName();
-            if(nameUser.equals(LoginUser.get(0))){
-                user.addChatHistory(chat);
-            }
-        }
-        }
+    }
 
-
-
-
-    @Override
+    /**
+     * Método que retorna el historial de un usuario
+     * @param username String
+     * @return chatHistory
+     */
     public String systemSynthesis(String username) {
-        for(User_21337525_ZunigaJofre user: RegisterUsers){
+        for(User_21337525_ZunigaJofre user: registerUsers){
             if(username.equals(user.getName())){
                 return user.getChatHistory();
             }
@@ -149,32 +228,24 @@ public class System_21337525_ZunigaJofre implements System_Interface_21337525_Zu
         return null;
     }
 
-    @Override
+    /**
+     * Método para simular una conversación con un System
+     * @param maxInteractions int
+     * @param seed int
+     */
     public void systemSimulate(Integer maxInteractions, Integer seed) {
         while(maxInteractions != 0){
-            seed = myRandom(seed);
-            String message = String.valueOf((seed % (ActualOptions.size()- 1))+ 1);
+            String message = String.valueOf((seed % (actualOptions.size()- 1))+ 1);
             systemTalk(message);
             maxInteractions -= 1;
         }
     }
-    public Integer myRandom(Integer x){
-        x = 1103515245 * x;
-        x = x + 12345;
-        x = x % 214748364;
-        return x;
-    }
-    public String toString(){
-        return Name + "," +InicialChatbotCodeLink + "," + RegisterUsers + "," + LoginUser + "," + ActualOptions + "," + Chatbots;
-    }
 
-    public Chatbot_21337525_ZunigaJofre getChatbotById(Integer id){
-        for(Chatbot_21337525_ZunigaJofre cb: Chatbots){
-            Integer cbId = cb.getId();
-            if(cbId.equals(id)){
-                return cb;
-            }
-        }
-        return null;
+    /**
+     * Método para imprimir un System en formato String
+     * @return String
+     */
+    public String toString(){
+        return name + "," + inicialChatbotCodeLink + "," + registerUsers + "," + loginUser + "," + actualOptions + "," + chatbots;
     }
 }
